b6d863ff875d18156c060827fc50a08d11b19eb8
zachariegauthier <zachariegauthier@cmail.carleton.ca>
2014-09-25 14:57:44 -0400

    added a removebuddy method that removes a buddyInformation based on the index.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -23,6 +23,20 @@ public class AddressBook
 		
 		return doesWork;
 	}
+	public boolean removeBuddy(int index)
+	{
+		boolean doesWork = true;
+		try 
+		{
+			book.remove(index);
+		}
+		catch(Exception e)
+		{
+			doesWork = false;
+		}
+		
+		return doesWork;
+	}
 	public boolean removeBuddy(BuddyInfo budInfo)
 	{
 		boolean doesWork = true; 
@@ -39,10 +53,17 @@ public class AddressBook
 		return doesWork;
 		
 	}
+	public AddressBook()
+	{
+		book = new ArrayList<BuddyInfo>();
+		
+	}
 	public static void main(String[] args)
 	{
+		
 		BuddyInfo buddy = new BuddyInfo("Justin","613-906-1234","666 Devil St,Hell");
 		AddressBook book = new AddressBook(); 
+		
 		if(!book.addBuddy(buddy)){
 			System.out.println("Add error"); 
 		}
